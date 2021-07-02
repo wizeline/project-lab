1de958800ad293c77c2d5021a708ed7ea04ec458
Joaquin Bravo Contreras <jackbravo@gmail.com>
2021-07-01 23:37:14 -0500

    generate first migration

---
+++ db/migrations/20210702041728_initial_migration/migration.sql
@@ -0,0 +1,248 @@
+-- CreateEnum
+CREATE TYPE "TokenType" AS ENUM ('RESET_PASSWORD');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT,
+    "role" TEXT NOT NULL DEFAULT E'USER',
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expiresAt" TIMESTAMP(3),
+    "handle" TEXT NOT NULL,
+    "hashedSessionToken" TEXT,
+    "antiCSRFToken" TEXT,
+    "publicData" TEXT,
+    "privateData" TEXT,
+    "userId" INTEGER,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "hashedToken" TEXT NOT NULL,
+    "type" "TokenType" NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "sentTo" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "JobTitles" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT,
+    "nameAbbreviation" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Locations" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProfileSkills" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "profileId" UUID NOT NULL,
+    "skillId" UUID NOT NULL,
+    "proficiency" TEXT,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profiles" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "searchCol" TEXT,
+    "avatarUrl" TEXT,
+    "locationId" UUID,
+    "jobTitleId" UUID,
+    "jobLevelTier" TEXT,
+    "jobLevelTitle" TEXT,
+    "department" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "terminatedAt" TIMESTAMP(3),
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectMembers" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "projectId" UUID NOT NULL,
+    "profileId" UUID NOT NULL,
+    "startDate" TIMESTAMP(3),
+    "endDate" TIMESTAMP(3),
+    "role" TEXT,
+    "status" TEXT,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectSkills" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "projectId" UUID NOT NULL,
+    "skillId" UUID NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectStatus" (
+    "name" TEXT NOT NULL,
+
+    PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "Projects" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "ownerId" UUID,
+    "name" TEXT NOT NULL,
+    "logo" TEXT,
+    "description" TEXT,
+    "valueStatement" TEXT,
+    "target" TEXT,
+    "demo" TEXT,
+    "repoUrl" TEXT,
+    "isApproved" BOOLEAN NOT NULL DEFAULT false,
+    "status" TEXT DEFAULT E'Draft',
+    "positions" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Skills" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session.handle_unique" ON "Session"("handle");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token.hashedToken_type_unique" ON "Token"("hashedToken", "type");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Locations.name_unique" ON "Locations"("name");
+
+-- CreateIndex
+CREATE INDEX "profile_skills_profile_id_idx" ON "ProfileSkills"("profileId");
+
+-- CreateIndex
+CREATE INDEX "profile_skills_skill_id_idx" ON "ProfileSkills"("skillId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profiles.email_unique" ON "Profiles"("email");
+
+-- CreateIndex
+CREATE INDEX "profiles_email_idx" ON "Profiles"("email");
+
+-- CreateIndex
+CREATE INDEX "profiles_job_title_id_idx" ON "Profiles"("jobTitleId");
+
+-- CreateIndex
+CREATE INDEX "profiles_location_id_idx" ON "Profiles"("locationId");
+
+-- CreateIndex
+CREATE INDEX "profiles_search_col_idx" ON "Profiles"("searchCol");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "project_members_project_id_profile_id_key" ON "ProjectMembers"("projectId", "profileId");
+
+-- CreateIndex
+CREATE INDEX "project_members_profile_id_idx" ON "ProjectMembers"("profileId");
+
+-- CreateIndex
+CREATE INDEX "project_members_project_id_idx" ON "ProjectMembers"("projectId");
+
+-- CreateIndex
+CREATE INDEX "project_skills_project_id_idx" ON "ProjectSkills"("projectId");
+
+-- CreateIndex
+CREATE INDEX "project_skills_skill_id_idx" ON "ProjectSkills"("skillId");
+
+-- CreateIndex
+CREATE INDEX "projects_owner_id_idx" ON "Projects"("ownerId");
+
+-- CreateIndex
+CREATE INDEX "projects_status_idx" ON "Projects"("status");
+
+-- CreateIndex
+CREATE INDEX "skills_name_idx" ON "Skills"("name");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileSkills" ADD FOREIGN KEY ("profileId") REFERENCES "Profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileSkills" ADD FOREIGN KEY ("skillId") REFERENCES "Skills"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profiles" ADD FOREIGN KEY ("jobTitleId") REFERENCES "JobTitles"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profiles" ADD FOREIGN KEY ("locationId") REFERENCES "Locations"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectMembers" ADD FOREIGN KEY ("profileId") REFERENCES "Profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectMembers" ADD FOREIGN KEY ("projectId") REFERENCES "Projects"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectSkills" ADD FOREIGN KEY ("projectId") REFERENCES "Projects"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectSkills" ADD FOREIGN KEY ("skillId") REFERENCES "Skills"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Projects" ADD FOREIGN KEY ("ownerId") REFERENCES "Profiles"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Projects" ADD FOREIGN KEY ("status") REFERENCES "ProjectStatus"("name") ON DELETE SET NULL ON UPDATE CASCADE;
