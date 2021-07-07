ace1949ccb44b2e17ab1ea6e72f7e6f8a5350829
Joaquin Bravo Contreras <jackbravo@gmail.com>
2021-07-06 20:58:55 -0500

    blitz generate crud/pages projects, needed fixing queries

---
+++ app/projects/queries/getProjects.ts
@@ -0,0 +1,30 @@
+import { paginate, resolver } from "blitz"
+import db, { Prisma } from "db"
+
+interface GetProjectsInput
+  extends Pick<Prisma.ProjectsFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
+
+export default resolver.pipe(
+  resolver.authorize(),
+  async ({ where, orderBy, skip = 0, take = 100 }: GetProjectsInput) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const {
+      items: projects,
+      hasMore,
+      nextPage,
+      count,
+    } = await paginate({
+      skip,
+      take,
+      count: () => db.projects.count({ where }),
+      query: (paginateArgs) => db.projects.findMany({ ...paginateArgs, where, orderBy }),
+    })
+
+    return {
+      projects,
+      nextPage,
+      hasMore,
+      count,
+    }
+  }
+)
