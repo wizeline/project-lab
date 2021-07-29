3af71dced4becf6b2498bba708bee161a0f1dda0
Joaquin Bravo Contreras <jackbravo@gmail.com>
2021-07-28 20:16:30 -0500

    Simplify _document, don't use https://emotion.sh/docs/ssr\#nextjs for now

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,15 +1,5 @@
-import * as React from "react"
-import { CacheProvider } from "@emotion/react"
-import createCache from "@emotion/cache"
 import { Document, Html, DocumentHead, Main, BlitzScript /*DocumentContext*/ } from "blitz"
-import createEmotionServer from "@emotion/server/create-instance"
-import theme from "app/core/components/theme"
-
-function getCache() {
-  const cache = createCache({ key: "css", prepend: true })
-  cache.compat = true
-  return cache
-}
+import theme from "app/core/utils/theme"
 
 class MyDocument extends Document {
   // Only uncomment if you need to customize this behaviour
@@ -39,65 +29,3 @@ class MyDocument extends Document {
     )
   }
 }
-
-// From https://github.com/mui-org/material-ui/blob/next/examples/nextjs/pages/_document.js
-// `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with static-site generation (SSG).
-MyDocument.getInitialProps = async (ctx) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
-  const originalRenderPage = ctx.renderPage
-
-  const cache = getCache()
-  const { extractCriticalToChunks } = createEmotionServer(cache)
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      // Take precedence over the CacheProvider in our custom _app.js
-      enhanceComponent: (Component) => (props) =>
-        (
-          <CacheProvider value={cache}>
-            <Component {...props} />
-          </CacheProvider>
-        ),
-    })
-
-  const initialProps = await Document.getInitialProps(ctx)
-  const emotionStyles = extractCriticalToChunks(initialProps.html)
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(" ")}`}
-      key={style.key}
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ))
-
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
-  }
-}
-
-export default MyDocument
