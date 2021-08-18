8d11dcb6ca07e42b58b59bb0fa9960934a10cad8
George wachira <kuria.wachira25@gmail.com>
2021-08-18 18:13:10 +0300

    Completed

--- terraform/distribution/main.tf
+++ terraform/distribution/main.tf
@@ -35,6 +35,75 @@ data "aws_acm_certificate" "certificate" {
   statuses = ["ISSUED"]
 }
 
+resource "aws_cloudfront_distribution" "distribution_prisma_studio" {
+  origin {
+    origin_id   = local.env_prefix == "default" ? "default" : local.env_prefix
+    domain_name = local.env_prefix == "default" ? "origin.labs.wizeline.com" : "${local.env_prefix}-origin.labs.wizeline.com"
+    custom_origin_config {
+      http_port              = 8080
+      https_port             = 8080
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["SSLv3"]
+    }
+  }
+  enabled = true
+  aliases = [local.env_prefix == "default" ? "prisma-studio.labs.wizeline.com" : "prisma-studio-${local.env_prefix}.labs.wizeline.com"]
+  tags    = local.resource_tags
+
+  ordered_cache_behavior {
+    path_pattern     = "*"
+    target_origin_id = local.env_prefix == "default" ? "default" : local.env_prefix
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+      headers = ["Host","Content-Type","Authorization"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.env_prefix == "default" ? "default" : local.env_prefix
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "all"
+      }
+
+      headers = ["Host","Content-Type","Authorization"]
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.certificate.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
 resource "aws_cloudfront_distribution" "distribution" {
   origin {
     origin_id   = local.env_prefix == "default" ? "default" : local.env_prefix
@@ -132,4 +201,19 @@ resource "aws_route53_record" "distribution_route53_record" {
   }
 
   depends_on      = [aws_cloudfront_distribution.distribution]
+}
+
+resource "aws_route53_record" "distribution_prisma_studio_route53_record" {
+  zone_id         = data.aws_route53_zone.route53_zone.zone_id
+  name            = local.env_prefix == "default" ? "prisma-studio" : "prisma-studio-${local.env_prefix}"
+  type            = "A"
+  allow_overwrite = true
+
+  alias {
+    name = aws_cloudfront_distribution.distribution_prisma_studio.domain_name
+    zone_id = aws_cloudfront_distribution.distribution_prisma_studio.hosted_zone_id
+    evaluate_target_health = true
+  }
+
+  depends_on      = [aws_cloudfront_distribution.distribution_prisma_studio]
 }
