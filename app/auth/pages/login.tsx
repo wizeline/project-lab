b7c4908d4e7f3c5d0dc920347a17ef018e9216eb
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-09-17 11:30:09 -0500

    Update login.tsx

--- app/auth/pages/login.tsx
+++ app/auth/pages/login.tsx
@@ -1,7 +1,7 @@
 import { BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import { LoginForm } from "app/auth/components/LoginForm"
-import { LoginPageContainer } from "./login.styles"
+import LoginPageContainer from "./login.styles"
 
 const LoginPage: BlitzPage = () => {
   return (
