b68d482e5f28797311438c228885a3ffbb45d833
Jesus Corona <jesus.corona@wizeline.com>
2022-02-10 08:45:29 -0600

    Add functionality for row edition, cancel and save

--- app/pages/manager.tsx
+++ app/pages/manager.tsx
@@ -14,8 +14,21 @@ import { Wrapper } from "./projects/projects.styles"
 import LabelsSelect from "app/core/components/LabelsSelect"
 import getLabels from "app/labels/queries/getLabels"
 import { Form, FormProps } from "app/core/components/Form"
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid"
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  useGridApiRef,
+  GridToolbarContainer,
+  GridActionsCellItem,
+} from "@mui/x-data-grid"
 import Button from "@mui/material/Button"
+import AddIcon from "@mui/icons-material/Add"
+import EditIcon from "@mui/icons-material/Edit"
+import DeleteIcon from "@mui/icons-material/DeleteOutlined"
+import SaveIcon from "@mui/icons-material/Save"
+import CancelIcon from "@mui/icons-material/Close"
+
 import projects from "./projects"
 import { ListItem } from "@mui/material"
 import createLabel from "app/labels/mutations/createLabel"
@@ -24,24 +37,43 @@ import { LabelForm, FORM_ERROR } from "app/labels/components/LabelForm"
 import getLabel from "app/labels/queries/getLabel"
 import updateLabel from "app/labels/mutations/updateLabel"
 import { useState } from "react"
-// Delete when cleaning up
-// const rows: GridRowsProp = [
-//   { id: 1, col1: "Summer2021", col2: "Delete" },
-//   { id: 2, col1: "Summer2022", col2: "Delete" },
-//   { id: 3, col1: "Summer2020", col2: "Delete" },
-// ]
 
-const LABELS_PER_PAGE = 100
+const RESULTS_QUEUED = 100
 
+// Tool bara elemento de edicion
+// const EditToolbar = (props) => {
+//   const { apiRef } = props
+
+//   const handleClick = () => {
+//     const id = "212121"
+//     apiRef.current.updateRows([{ id, isNew: true }])
+//     apiRef.current.setRowMode(id, "edit")
+//     // Wait for the grid to render with the new row
+//     setTimeout(() => {
+//       apiRef.current.scrollToIndexes({
+//         rowIndex: apiRef.current.getRowsCount() - 1,
+//       })
+
+//       apiRef.current.setCellFocus(id, "name")
+//     })
+//   }
+//   return (
+//     <GridToolbarContainer>
+//       <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+//         Add record
+//       </Button>
+//     </GridToolbarContainer>
+//   )
+// }
+
+// EditToolbar.propTypes = {
+//   apiRef: PropTypes.shape({
+//     current: PropTypes.object.isRequired,
+//   }).isRequired,
+// }
+
+// Button for submitting edition
 const SomeButton = (puid) => {
-  // Add mutation for label
-  // const [label, { setQueryData }] = useQuery(
-  //   getLabel,
-  //   { id: labelId },
-  //   {
-  //     staleTime: Infinity,
-  //   }
-  // )
   const [updateLabelMutation] = useMutation(updateLabel)
   const editLabelInfo = async (id: string, values: any) => {
     try {
@@ -52,7 +84,6 @@ const SomeButton = (puid) => {
       // await setQueryData(updated)
       console.log("Values were UPDATED for label")
       // router.push(Routes.ManagerPage())
-      // router.push(Routes.ShowLabelPage({ labelId: updated.id }))
     } catch (error: any) {
       console.error(error)
       return {
@@ -61,6 +92,7 @@ const SomeButton = (puid) => {
     }
   }
 
+  // Handle updates of rows
   const manageEdition = async (puid: any) => {
     console.dir(puid)
     console.log(`Edit: ${puid.row.id}`)
@@ -86,91 +118,236 @@ const SomeButton = (puid) => {
   )
 }
 
-const ManagerPage: BlitzPage = () => {
+const LabelsDataGrid = () => {
   const router = useRouter()
   const page = Number(router.query.page) || 0
   const [createLabelMutation] = useMutation(createLabel)
-  const [submitValue, getSubmitValue] = useState("")
+  const [{ labels, hasMore }] = usePaginatedQuery(
+    getLabels,
+    {
+      orderBy: { name: "asc" },
+      skip: RESULTS_QUEUED * page,
+      take: RESULTS_QUEUED,
+    },
+    { staleTime: 200, refetchInterval: 200 }
+  )
 
-  // Turn into its own compoennet
-  const [{ labels, hasMore }] = usePaginatedQuery(getLabels, {
-    orderBy: { name: "asc" },
-    skip: LABELS_PER_PAGE * page,
-    take: LABELS_PER_PAGE,
-  })
+  // const [{ labels }] = useQuery(getLabels, {
+  //   orderBy: { name: "asc" },
+  // })
+
+  const [updateLabelMutation] = useMutation(updateLabel)
+  const editLabelInfo = async (id: string, values: any) => {
+    try {
+      const updated = await updateLabelMutation({
+        id: id,
+        ...values,
+      })
+      // await setQueryData(updated)
+      console.log("Values were UPDATED for label")
+      // router.push(Routes.ManagerPage())
+    } catch (error: any) {
+      console.error(error)
+      return {
+        [FORM_ERROR]: error.toString(),
+      }
+    }
+  }
+
+  // Set handles for interactions
+  const apiRef = useGridApiRef()
+
+  const handleRowEditStart = (params, event) => {
+    event.defaultMuiPrevented = true
+  }
+
+  const handleRowEditStop = (params, event) => {
+    event.defaultMuiPrevented = true
+  }
+
+  const handleCellFocusOut = (params, event) => {
+    event.defaultMuiPrevented = true
+  }
+
+  const handleEditClick = (id) => {
+    // event.stopPropagation()
+    // console.dir(id.api.getCellValue)
+    id.api.setRowMode(id.row.id, "edit")
+    console.dir(id)
+
+    // apiRef.current.setRowMode(id, "edit")
+  }
+  // const handleSaveClick = (id) => async (event) => {
+  //   event.stopPropagation()
+  //   // Wait for the validation to run
+  //   const isValid = await apiRef.current.commitRowChange(id)
+  //   if (isValid) {
+  //     apiRef.current.setRowMode(id, "view")
+  //     const row = apiRef.current.getRow(id)
+  //     apiRef.current.updateRows([{ ...row, isNew: false }])
+  //   }
+  // }
+  const handleSaveClick = async (idRef) => {
+    // event.stopPropagation()
+    // Wait for the validation to run
+    const isValid = await idRef.api.commitRowChange(idRef.row.id)
+    if (isValid) {
+      const row = idRef.api.getRow(idRef.row.id)
+      let id = idRef.row.id
+      let newLabel = idRef.api.getCellValue(id, "name")
+      await editLabelInfo(id, { name: newLabel })
+      idRef.api.updateRows([{ ...row, isNew: false }])
+      idRef.api.setRowMode(id, "view")
+      console.log(`New Label Value: ${newLabel}`)
+      router.push(Routes.ManagerPage())
+    }
+  }
+
+  const handleDeleteClick = (id) => (event) => {
+    event.stopPropagation()
+    apiRef.current.updateRows([{ id, _action: "delete" }])
+  }
+
+  // const handleCancelClick = (id) => (event) => {
+  //   event.stopPropagation()
+  //   apiRef.current.setRowMode(id, "view")
+
+  //   const row = apiRef.current.getRow(id)
+  //   if (row && row.isNew) {
+  //     apiRef.current.updateRows([{ id, _action: "delete" }])
+  //   }
+  // }
+  const handleCancelClick = (id) => {
+    // event.stopPropagation()
+    id.api.setRowMode(id.row.id, "view")
+
+    // const row = apiRef.current.getRow(id)
+    // if (row && row.isNew) {
+    //   apiRef.current.updateRows([{ id, _action: "delete" }])
+    // }
+  }
 
   const rows: GridRowsProp = labels.map((item, key) => ({
     id: item.id,
     name: item.name,
-    edit: "delete",
-    // col3: item.id,
+    // edit: "delete",
   }))
-
-  const getInfoValue = (params) => {
-    return params.data.name
-  }
-
-  const columns: GridColDef[] = [
+  const columns = [
     { field: "name", headerName: "Name", width: 300, editable: true },
-    {
-      field: "edit",
-      headerName: "Edition",
-      width: 150,
-      renderCell: SomeButton,
-      // valueGetter: getInfoValue,
-    },
-    // { field: "col3", headerName: "ID", width: 150 },
-  ]
+    // {
+    //   field: "edit",
+    //   headerName: "Edition",
+    //   width: 150,
+    //   renderCell: SomeButton,
+    // },
 
-  const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
-  const goToNextPage = () => router.push({ query: { page: page + 1 } })
+    {
+      field: "actions",
+      // type: "actions",
+      headerName: "Actions",
+      width: 300,
+      cellClassName: "actions",
+      renderCell: (id) => {
+        const isInEditMode = id.api.getRowMode(id.row.id) === "edit"
+        if (isInEditMode) {
+          return (
+            <>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                onClick={() => handleCancelClick(id)}
+                style={{ marginLeft: 16, backgroundColor: "#AF2E33" }}
+              >
+                Cancel
+              </Button>
+
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                onClick={() => handleSaveClick(id)}
+                style={{ marginLeft: 16, backgroundColor: "#2ea6af" }}
+              >
+                Save
+              </Button>
+            </>
+          )
+        }
+
+        return (
+          <>
+            {/* <GridActionsCellItem
+              icon={<EditIcon />}
+              label="Edit"
+              className="textPrimary"
+              // onClick={handleEditClick(id)}
+              onClick={() => handleEditClick(id)}
+              color="inherit"
+            /> */}
+
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              onClick={() => handleEditClick(id)}
+              style={{ marginLeft: 16, backgroundColor: "#AF2E33" }}
+            >
+              Edit
+            </Button>
+          </>
+        )
+      },
+    },
+  ]
 
   return (
     <div>
+      <h2>Labels</h2>
+      {/* <div style={{ height: 300, width: "100%" }}> */}
+      <div style={{ display: "flex", width: "100%", height: 300 }}>
+        <div style={{ flexGrow: 1 }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            // apiRef={apiRef}
+            rowsPerPageOptions={[4, 8, 12]}
+            pageSize={4}
+            editMode="row"
+            onRowEditStart={handleRowEditStart}
+            onRowEditStop={handleRowEditStop}
+            onCellFocusOut={handleCellFocusOut}
+          />
+        </div>
+      </div>
+
+      <h3>Create Label</h3>
+      <LabelForm
+        submitText="Create Label"
+        onSubmit={async (values) => {
+          try {
+            const label = await createLabelMutation(values)
+            console.log("Info was saved")
+            router.push(Routes.ManagerPage())
+          } catch (error: any) {
+            console.error(error)
+            return {
+              [FORM_ERROR]: error.toString(),
+            }
+          }
+        }}
+      />
+    </div>
+  )
+}
+
+const ManagerPage: BlitzPage = () => {
+  return (
+    <div>
       <Header title="Manager" />
-      {/* Check if the search option in the header should be a prop, in order to not show it in this type pf pages */}
       <Wrapper className="homeWrapper">
         <CardBox title="Parameters">
-          <div>
-            <h2>Labels</h2>
-            {/* <div>
-              {labels.map((item, key) => (
-                <p key={key}>{item.name}</p>
-              ))}
-            </div> */}
-            <div style={{ height: 300, width: "100%" }}>
-              <DataGrid
-                rows={rows}
-                columns={columns}
-                rowsPerPageOptions={[4, 8, 12]}
-                pageSize={4}
-              />
-            </div>
-
-            <h3>Create Label</h3>
-            <LabelForm
-              submitText="Create Label"
-              // TODO use a zod schema for form validation
-              //  - Tip: extract mutation's schema into a shared `validations.ts` file and
-              //         then import and use it here
-              // schema={CreateLabel}
-              // initialValues={{}}
-              onSubmit={async (values) => {
-                try {
-                  const label = await createLabelMutation(values)
-                  // router.push(Routes.ShowLabelPage({ labelId: label.id }))
-                  console.log("Info was saved")
-                  router.push(Routes.ManagerPage())
-                  // labels.push(values.name)
-                } catch (error: any) {
-                  console.error(error)
-                  return {
-                    [FORM_ERROR]: error.toString(),
-                  }
-                }
-              }}
-            />
-          </div>
+          <LabelsDataGrid />
         </CardBox>
       </Wrapper>
     </div>
