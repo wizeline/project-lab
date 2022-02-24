8ed3a3afd098e3ea2023455e821d54c88ac0a7c3
juanan0309 <juan.quiroz@wizeline.com>
2022-02-24 16:20:22 -0500

    Add admins query and listing

--- app/core/components/AdminsDataGrid/index.tsx
+++ app/core/components/AdminsDataGrid/index.tsx
@@ -13,16 +13,19 @@ import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import { adminRoleName } from "app/core/utils/constants"
 import themeWize from "app/core/utils/themeWize"
 
+import getAdminUsers from "../../../users/queries/getAdminUsers"
+
 const GridEditToolbar = (props) => {
   const { setRows, createButtonText, user } = props
   const handleAddClick = () => {
     const id = "new-value"
     const newName = ""
+    const newEmail = ""
     setRows((prevRows) => {
       if (prevRows.find((rowValue) => rowValue.id === "new-value")) {
         return [...prevRows]
       }
-      return [...prevRows, { id, name: newName, isNew: true }]
+      return [...prevRows, { id, name: newName, email: newEmail, isNew: true }]
     })
   }
   if (user?.role !== adminRoleName) return <></>
@@ -43,19 +46,37 @@ const GridEditToolbar = (props) => {
 const AdminsDataGrid = () => {
   const user = useCurrentUser()
   const createButtonText = "Add New Admin"
+  const [admins] = useQuery(getAdminUsers, null)
 
-  const columns = [
-    { field: "name", headerName: "Name", width: 300, editable: false },
-    { field: "email", headerName: "Email", width: 300, editable: false },
-  ]
+  // Set handles for interactions
 
-  const admins = [
-    {
-      id: "1",
-      name: "tester",
-      email: "test@test.com",
-    },
-  ]
+  const handleRowEditStart = (event) => {
+    event.defaultMuiPrevented = true
+  }
+
+  const handleRowEditStop = (event) => {
+    event.defaultMuiPrevented = true
+  }
+
+  const handleCellFocusOut = (event) => {
+    event.defaultMuiPrevented = true
+  }
+
+  const handleCancelClick = async (idRef) => {
+    const id = idRef.row.id
+    idRef.api.setRowMode(id, "view")
+
+    const row = idRef.api.getRow(id)
+    if (row && row.isNew) {
+      await idRef.api.updateRows([{ id, _action: "delete" }])
+      setRows((prevRows) => {
+        const rowToDeleteIndex = prevRows.length - 1
+        return [...rows.slice(0, rowToDeleteIndex), ...rows.slice(rowToDeleteIndex + 1)]
+      })
+    }
+  }
+  const handleSaveClick = (id) => {}
+  const handleDeleteClick = (id) => {}
 
   const [rows, setRows] = useState(() =>
     admins.map((item) => ({
@@ -64,6 +85,62 @@ const AdminsDataGrid = () => {
       email: item.email,
     }))
   )
+
+  const columns = [
+    { field: "email", headerName: "Email", width: 300, editable: true },
+    { field: "name", headerName: "Name", width: 300, editable: false },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 300,
+      renderCell: (idRef: any) => {
+        if (idRef.row.id === "new-value") {
+          idRef.api.setRowMode(idRef.row.id, "edit")
+          idRef.api.setCellFocus(idRef.row.id, "name")
+        }
+        const isInEditMode = idRef.api.getRowMode(idRef.row.id) === "edit"
+        if (user?.role !== adminRoleName) {
+          return <></>
+        }
+        if (isInEditMode) {
+          return (
+            <>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                onClick={() => handleCancelClick(idRef)}
+                style={{ marginLeft: 16 }}
+              >
+                <CancelIcon color="inherit" />
+              </Button>
+
+              <Button
+                variant="contained"
+                color="secondary"
+                size="small"
+                onClick={() => handleSaveClick(idRef)}
+                style={{ marginLeft: 16 }}
+              >
+                <SaveIcon color="inherit" />
+              </Button>
+            </>
+          )
+        }
+        return (
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={() => handleDeleteClick(idRef)}
+            style={{ marginLeft: 16 }}
+          >
+            <DeleteIcon color="inherit" />
+          </Button>
+        )
+      },
+    },
+  ]
 
   return (
     <div style={{ display: "flex", width: "100%", height: "70vh" }}>
@@ -75,6 +152,9 @@ const AdminsDataGrid = () => {
             rowsPerPageOptions={[50]}
             pageSize={50}
             editMode="row"
+            onRowEditStart={handleRowEditStart}
+            onRowEditStop={handleRowEditStop}
+            onCellFocusOut={handleCellFocusOut}
             components={{
               Toolbar: GridEditToolbar,
             }}
