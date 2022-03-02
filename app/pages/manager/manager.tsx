879f5e582cf38513fa930f440a4a82aa42dfa06a
Leonardo Hervert <leonardo.hervert@wizeline.com>
2022-03-02 14:24:14 -0600

    adding missing file

--- app/pages/manager/manager.tsx
+++ app/pages/manager/manager.tsx
@@ -2,7 +2,7 @@ import { BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import Header from "app/core/layouts/Header"
 import CardBox from "app/core/components/CardBox"
-import { Wrapper } from "../projects/projects.styles"
+import Wrapper from "../projects/projects.styles"
 
 import { Box, Tabs } from "@mui/material"
 import { TabStyles, EditPanelsStyles } from "app/projects/components/Styles/TabStyles.component"
