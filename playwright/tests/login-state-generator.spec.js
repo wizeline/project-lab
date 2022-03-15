da42111bd68f9a0e70a6f29c55981a2c73a1a7fb
florethge <deadlypinkshadeXX@gmail.com>
2022-03-15 13:13:41 -0600

    Login Auth template and tests (#251)
    * playwright implementation and jest config adjustments
    
    * modified pkg json to point to ts configuration
    
    * Added login authentication tests
    
    * Added login authentication tests
    
    Co-authored-by: Brayan Torres <brayan.torres@Brayans-MacBook-Pro.local>
    Co-authored-by: Floreth Gonzalez Espinoza <floreth.gonzalez@Floreth-Gonzalezs-MacBook-Pro.local>

---
+++ playwright/tests/login-state-generator.spec.js
@@ -0,0 +1,17 @@
+// @ts-check
+const { test, expect } = require("@playwright/test")
+
+test("Login test flow", async ({ page, context }) => {
+  await page.goto("https://labs.wizeline.com/")
+
+  await page.locator("text=Login with your Wizeline email account").click()
+  await page.locator('button:has-text("Continue with Google")').click()
+  await page.locator('[aria-label="Correo electrónico o teléfono"]').fill(process.env.OKTA_USERNAME)
+  await page.locator('button:has-text("Siguiente")').click()
+  await page.locator('input[name="username"]').fill(process.env.OKTA_USERNAME)
+  await 1
+  await page.locator("#okta-signin-password").fill(process.env.OKTA_PASSWORD)
+  await page.locator('input:has-text("Sign In")').click()
+  await page.pause()
+  await context.storageState({ path: "state.json" })
+})
