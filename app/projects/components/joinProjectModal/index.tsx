aed1d42ddf689bd05ee7e1f1f7cd9ba2f41374ac
Alejandro <jesus.martinez@wizeline.com>
2022-03-16 15:33:34 -0600

    chore(schema): added practicedSkills field to ProjectMembers model. chore(seeds): updated jesus.martinez profile, and Project Lab projectMembers

--- app/projects/components/joinProjectModal/index.tsx
+++ app/projects/components/joinProjectModal/index.tsx
@@ -3,8 +3,10 @@ import { z } from "zod"
 import { Form } from "app/core/components/Form"
 import LabeledTextField from "app/core/components/LabeledTextField"
 import ModalBox from "app/core/components/ModalBox"
-import { Button } from "@mui/material"
+import Button from "@mui/material/Button"
+
 import useProjectMembers from "app/projects/hooks/useProjectMembers"
+import { SkillsSelect } from "app/core/components/SkillsSelect"
 import { Grid, FormDivContainer, CommitmentDivContainer } from "./joinProjectModal.styles"
 import { Routes, useRouter } from "blitz"
 
@@ -19,6 +21,7 @@ export const JoinFields = z.object({
   hoursPerWeek: z.string().refine((val) => !val || /^\d+$/.test(val), {
     message: "Value must be an integer",
   }),
+  skills: z.any(),
 })
 
 const JoinProjectModal = (props: IProps) => {
@@ -67,6 +70,9 @@ const JoinProjectModal = (props: IProps) => {
               label="Hours per Week"
               outerProps={{ style: { marginTop: 10, marginBottom: 20 } }}
             />
+
+            <p className="question">Which skills are you practicing?</p>
+            {/*<SkillsSelect name="skills" label="Skills" />*/}
           </FormDivContainer>
 
           <CommitmentDivContainer>
