faf27d7335daf1989c50f03b579fae9d1350a58d
Joaquin Bravo Contreras <jackbravo@gmail.com>
2022-04-18 11:46:20 -0500

    Add project verions as well

--- db/migrations/20220417031606_project_members_versions_customized/migration.sql
+++ db/migrations/20220417031606_project_members_versions_customized/migration.sql
@@ -13,6 +13,33 @@ CREATE TABLE "ProjectMembersVersions" (
     FOREIGN KEY ("projectId") REFERENCES "Projects" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "ProjectsVersions" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "ownerId" TEXT,
+    "name" TEXT NOT NULL,
+    "logo" TEXT,
+    "description" TEXT,
+    "valueStatement" TEXT,
+    "target" TEXT,
+    "demo" TEXT,
+    "repoUrl" TEXT,
+    "slackChannel" TEXT,
+    "isApproved" BOOLEAN NOT NULL DEFAULT false,
+    "status" TEXT DEFAULT 'Idea Submitted',
+    "categoryName" TEXT DEFAULT 'Experimenter',
+    "positions" TEXT,
+    "searchSkills" TEXT NOT NULL DEFAULT '',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "membersCount" INTEGER NOT NULL DEFAULT 0,
+    "votesCount" INTEGER NOT NULL DEFAULT 0,
+    "isArchived" BOOLEAN NOT NULL DEFAULT false,
+    FOREIGN KEY ("ownerId") REFERENCES "Profiles" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("status") REFERENCES "ProjectStatus" ("name") ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY ("categoryName") REFERENCES "Category" ("name") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_ProjectMembers" (
@@ -45,7 +72,47 @@ CREATE INDEX "project_members_v_profile_id_idx" ON "ProjectMembersVersions"("pro
 -- CreateIndex
 CREATE INDEX "project_members_v_project_id_idx" ON "ProjectMembersVersions"("projectId");
 
---- Manually added
+-- CreateIndex
+CREATE INDEX "projects_version_owner_id_idx" ON "ProjectsVersions"("ownerId");
+
+-- CreateIndex
+CREATE INDEX "projects_version_status_idx" ON "ProjectsVersions"("status");
+
+-- CreateIndex
+CREATE INDEX "projects_version_category_idx" ON "ProjectsVersions"("categoryName");
+
+-- CreateIndex
+CREATE INDEX "projects_version_isArchived_idx" ON "ProjectsVersions"("isArchived");
+
+--- Manually added for ProjectsVersions
+CREATE TRIGGER IF NOT EXISTS "projects_v_insert" AFTER INSERT ON "Projects" BEGIN
+    INSERT INTO "ProjectsVersions" ("ownerId", "name", "logo", "description", "valueStatement", "target", "demo", "repoUrl", "slackChannel", "isApproved", "status", "categoryName", "positions", "searchSkills", "isArchived",
+      "membersCount", "votesCount")
+    VALUES (NEW.ownerId, NEW.name, NEW.logo, NEW.description, NEW.valueStatement, NEW.target, NEW.demo, NEW.repoUrl, NEW.slackChannel, NEW.isApproved, NEW.status, NEW.categoryName, NEW.positions, NEW.searchSkills, NEW.isArchived,
+      (SELECT COUNT(*) FROM ProjectMembers WHERE ProjectMembers.projectId = NEW.id),
+      (SELECT COUNT(*) FROM Vote WHERE Vote.projectId = NEW.id)
+    );
+END;
+
+CREATE TRIGGER IF NOT EXISTS "projects_v_update" AFTER UPDATE ON "Projects" BEGIN
+    INSERT INTO "ProjectsVersions" ("ownerId", "name", "logo", "description", "valueStatement", "target", "demo", "repoUrl", "slackChannel", "isApproved", "status", "categoryName", "positions", "searchSkills", "isArchived",
+      "membersCount", "votesCount")
+    VALUES (NEW.ownerId, NEW.name, NEW.logo, NEW.description, NEW.valueStatement, NEW.target, NEW.demo, NEW.repoUrl, NEW.slackChannel, NEW.isApproved, NEW.status, NEW.categoryName, NEW.positions, NEW.searchSkills, NEW.isArchived,
+      (SELECT COUNT(*) FROM ProjectMembers WHERE ProjectMembers.projectId = NEW.id),
+      (SELECT COUNT(*) FROM Vote WHERE Vote.projectId = NEW.id)
+    );
+END;
+
+CREATE TRIGGER IF NOT EXISTS "projects_v_delete" AFTER DELETE ON "Projects" BEGIN
+    INSERT INTO "ProjectsVersions" ("ownerId", "name", "logo", "description", "valueStatement", "target", "demo", "repoUrl", "slackChannel", "isApproved", "status", "categoryName", "positions", "searchSkills", "isArchived",
+      "membersCount", "votesCount")
+    VALUES (OLD.ownerId, OLD.name, OLD.logo, OLD.description, OLD.valueStatement, OLD.target, OLD.demo, OLD.repoUrl, OLD.slackChannel, OLD.isApproved, OLD.status, OLD.categoryName, OLD.positions, OLD.searchSkills, OLD.isArchived,
+      0,
+      0
+    );
+END;
+
+--- Manually added for ProjectMembersVersions
 CREATE TRIGGER IF NOT EXISTS "project_members_v_insert" AFTER INSERT ON "ProjectMembers" BEGIN
     INSERT INTO "ProjectMembersVersions"("projectId", "profileId", "hoursPerWeek", "role", "active", "practicedSkills")
     VALUES (new.projectId, new.profileId, new.hoursPerWeek, new.role, new.active, "");
