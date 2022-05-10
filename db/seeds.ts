3fd914ab58d60e1f349f6b5be3c12be0765c03a7
juanan0309 <juan.quiroz@wizeline.com>
2022-05-10 17:55:08 -0500

    update prisma version in yarn.lock and add logs to seeding

--- db/seeds.ts
+++ db/seeds.ts
@@ -501,190 +501,194 @@ const seed = async () => {
       department: "Engineering",
     },
   })
-  await db.projects.upsert({
-    where: { name: "Proposal Hunt" },
-    update: {},
-    create: {
-      name: "Proposal Hunt",
-      owner: { connect: { id: jp.id } },
-      description: "Visibility of projects",
-      valueStatement: "Socialize them with comments, votes, simple search.",
-      target: "All wizeliners",
-      projectStatus: { connect: { name: "Need SME Review" } },
-      searchSkills: "TypeScript, SQLite, React",
-      skills: {
-        connect: [
-          { id: "4b3d0d73-c3a6-457a-bd81-6b7b2be13373" },
-          { id: "b70ca72b-0a56-47f8-af2d-695c79673a68" },
-          { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
-        ],
-      },
-      labels: { connect: [{ name: "Innovation Camp 2020" }] },
-      projectMembers: {
-        create: [
-          {
-            profile: { connect: { id: jp.id } },
-            hoursPerWeek: 3,
-            role: "Tech Lead",
-          },
-        ],
-      },
-      stages: {
-        create: projectStages,
-      },
-    },
-  })
-  await db.projects.upsert({
-    where: { name: "Project Lab" },
-    update: {},
-    create: {
-      name: "Project Lab",
-      owner: { connect: { id: jbc.id } },
-      category: { connect: { name: "Value Creator" } },
-      description: "Visibility of initiatives. Follow up of skills and work from Wizeliners",
-      valueStatement: "New WCI board",
-      target: "All wizeliners",
-      projectStatus: { connect: { name: "Need SME Review" } },
-      searchSkills: "TypeScript, SQLite, React",
-      skills: {
-        connect: [
-          { id: "4b3d0d73-c3a6-457a-bd81-6b7b2be13373" },
-          { id: "b70ca72b-0a56-47f8-af2d-695c79673a68" },
-          { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
-        ],
-      },
-      disciplines: {
-        connect: [{ name: "UX Designer" }, { name: "Backend" }, { name: "Frontend" }],
-      },
-      labels: { connect: [{ name: "Innovation Camp 2020" }, { name: "Innovation Camp 2021" }] },
-      projectMembers: {
-        create: [
-          {
-            profile: { connect: { id: jbc.id } },
-            hoursPerWeek: 3,
-            role: "Frontend",
-          },
-          {
-            profile: { connect: { id: tejeda.id } },
-            hoursPerWeek: 3,
-            role: "Backend",
-          },
-          {
-            profile: { connect: { id: hramirez.id } },
-            hoursPerWeek: 3,
-            role: "Backend",
-          },
-          {
-            profile: { connect: { id: george.id } },
-            hoursPerWeek: 3,
-            role: "SRE",
-          },
-          {
-            profile: { connect: { id: jquiroz.id } },
-            hoursPerWeek: 3,
-            role: "Frontend",
-          },
-          {
-            profile: { connect: { id: jamj.id } },
-            hoursPerWeek: 3,
-            role: "Frontend",
-            practicedSkills: {
-              connect: [
-                { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
-                { id: "4b3d0d73-c3a6-457a-bd81-6b7b2be13373" },
-              ],
+
+  console.info("Profiles created, starting projects upsert")
+
+  try {
+    await db.projects.upsert({
+      where: { name: "Proposal Hunt" },
+      update: {},
+      create: {
+        name: "Proposal Hunt",
+        owner: { connect: { id: jp.id } },
+        description: "Visibility of projects",
+        valueStatement: "Socialize them with comments, votes, simple search.",
+        target: "All wizeliners",
+        projectStatus: { connect: { name: "Need SME Review" } },
+        searchSkills: "TypeScript, SQLite, React",
+        skills: {
+          connect: [
+            { id: "4b3d0d73-c3a6-457a-bd81-6b7b2be13373" },
+            { id: "b70ca72b-0a56-47f8-af2d-695c79673a68" },
+            { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
+          ],
+        },
+        labels: { connect: [{ name: "Innovation Camp 2020" }] },
+        projectMembers: {
+          create: [
+            {
+              profile: { connect: { id: jp.id } },
+              hoursPerWeek: 3,
+              role: "Tech Lead",
             },
-          },
-        ],
+          ],
+        },
+        stages: {
+          create: projectStages,
+        },
       },
-      stages: {
-        create: projectStages,
+    })
+    await db.projects.upsert({
+      where: { name: "Project Lab" },
+      update: {},
+      create: {
+        name: "Project Lab",
+        owner: { connect: { id: jbc.id } },
+        category: { connect: { name: "Value Creator" } },
+        description: "Visibility of initiatives. Follow up of skills and work from Wizeliners",
+        valueStatement: "New WCI board",
+        target: "All wizeliners",
+        projectStatus: { connect: { name: "Need SME Review" } },
+        searchSkills: "TypeScript, SQLite, React",
+        skills: {
+          connect: [
+            { id: "4b3d0d73-c3a6-457a-bd81-6b7b2be13373" },
+            { id: "b70ca72b-0a56-47f8-af2d-695c79673a68" },
+            { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
+          ],
+        },
+        disciplines: {
+          connect: [{ name: "UX Designer" }, { name: "Backend" }, { name: "Frontend" }],
+        },
+        labels: { connect: [{ name: "Innovation Camp 2020" }, { name: "Innovation Camp 2021" }] },
+        projectMembers: {
+          create: [
+            {
+              profile: { connect: { id: jbc.id } },
+              hoursPerWeek: 3,
+              role: "Frontend",
+            },
+            {
+              profile: { connect: { id: tejeda.id } },
+              hoursPerWeek: 3,
+              role: "Backend",
+            },
+            {
+              profile: { connect: { id: hramirez.id } },
+              hoursPerWeek: 3,
+              role: "Backend",
+            },
+            {
+              profile: { connect: { id: george.id } },
+              hoursPerWeek: 3,
+              role: "SRE",
+            },
+            {
+              profile: { connect: { id: jquiroz.id } },
+              hoursPerWeek: 3,
+              role: "Frontend",
+            },
+            {
+              profile: { connect: { id: jamj.id } },
+              hoursPerWeek: 3,
+              role: "Frontend",
+              practicedSkills: {
+                connect: [
+                  { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
+                  { id: "4b3d0d73-c3a6-457a-bd81-6b7b2be13373" },
+                ],
+              },
+            },
+          ],
+        },
+        stages: {
+          create: projectStages,
+        },
       },
-    },
-  })
-  await db.projects.upsert({
-    where: { name: "Wizepace" },
-    update: {},
-    create: {
-      name: "Wizepace",
-      owner: { connect: { id: ev.id } },
-      category: { connect: { name: "Value Creator" } },
-      description:
-        "The global pandemic has changed how we view the workplace and the needs we have. However, the office is still one of many tools that Wizeliners have to facilitate collaboration and social engagement. Acknowledging the previous facts, we need to think of a way to come back to our workplaces",
-      valueStatement: `This solution is called Wizeline + Space = Wizepace. We will implement a solution that:
+    })
+    await db.projects.upsert({
+      where: { name: "Wizepace" },
+      update: {},
+      create: {
+        name: "Wizepace",
+        owner: { connect: { id: ev.id } },
+        category: { connect: { name: "Value Creator" } },
+        description:
+          "The global pandemic has changed how we view the workplace and the needs we have. However, the office is still one of many tools that Wizeliners have to facilitate collaboration and social engagement. Acknowledging the previous facts, we need to think of a way to come back to our workplaces",
+        valueStatement: `This solution is called Wizeline + Space = Wizepace. We will implement a solution that:
       * Gives Wizeliners the ability to: Book a desk and Visit different offices or locations.
       * Fulfills the expectations of hybrid work: from home and the office.
       * Helps the Facilities Team to maximize the use of our workspace and comply with health and safety regulations. ",
       target: "All wizeliners`,
-      projectStatus: { connect: { name: "Need SME Review" } },
-      searchSkills: "React, AWS, Ruby",
-      skills: {
-        connect: [
-          { id: "6c859cfb-942c-40c8-b0b3-37de3e739c29" },
-          { id: "66fc5f5f-3be4-453f-8978-8cc3334adda1" },
-          { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
-        ],
+        projectStatus: { connect: { name: "Need SME Review" } },
+        searchSkills: "React, AWS, Ruby",
+        skills: {
+          connect: [
+            { id: "6c859cfb-942c-40c8-b0b3-37de3e739c29" },
+            { id: "66fc5f5f-3be4-453f-8978-8cc3334adda1" },
+            { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
+          ],
+        },
+        disciplines: { connect: [{ name: "Backend" }] },
+        labels: { connect: [{ name: "Innovation Camp 2021" }] },
+        projectMembers: {
+          create: [
+            {
+              profile: { connect: { id: ev.id } },
+              hoursPerWeek: 3,
+              role: "Tech Lead",
+            },
+          ],
+        },
+        stages: {
+          create: projectStages,
+        },
       },
-      disciplines: { connect: [{ name: "Backend" }] },
-      labels: { connect: [{ name: "Innovation Camp 2021" }] },
-      projectMembers: {
-        create: [
-          {
-            profile: { connect: { id: ev.id } },
-            hoursPerWeek: 3,
-            role: "Tech Lead",
-          },
-        ],
+    })
+    await db.projects.upsert({
+      where: { name: "WizeRunner" },
+      update: {},
+      create: {
+        name: "WizeRunner",
+        owner: { connect: { id: b.id } },
+        description:
+          "With the current pandemic situation, people are advised not to gather in large numbers for any sporting events such as mass running tournaments. Moreover, many individuals do not necessarily have to compete with others since competing against themselves offers the greatest reward. From those reasons, we came up with the idea to build a system not only for Wizeliners but also for the community so that everyone can stay motivated and boost their competitive spirit through running challenges and virtual races.",
+        valueStatement:
+          "Nowadays, there are a number of systems that allow users to participate in running challenges or participate in virtual races with some limitations. The first drawback is that most of these systems are not free. Users have to pay to be able to participate in virtual races or to be able to join public or private running challenges individually or with a team. The second limitation is that some of the paid systems, such as the ChallengeRunner, have a rather monotonous and boring design and reporting. That may reduce the users' motivation when using the app. Finally, most systems only support one of two functions, either participating in running challenges or participating in virtual races. In other words, to use both of those functions, users have to use and switch between two separate systems which is complex and costly. That motivates us to build a centralized system for Wizeliners to improve physical and mental health through virtual running functions.",
+        target: "All wizeliners",
+        projectStatus: { connect: { name: "Idea Submitted" } },
+        searchSkills: "Java, PostgreSQL, React",
+        skills: {
+          connect: [
+            { id: "f88e6170-9382-4a89-87a4-b820e4b0ac33" },
+            { id: "d7e95b76-6fc0-4e11-9674-61233cbe60ca" },
+            { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
+          ],
+        },
+        labels: { connect: [{ name: "Innovation Camp 2021" }] },
+        projectMembers: {
+          create: [
+            {
+              profile: { connect: { id: b.id } },
+              hoursPerWeek: 3,
+              role: "Tech Lead",
+            },
+          ],
+        },
+        stages: {
+          create: projectStages,
+        },
       },
-      stages: {
-        create: projectStages,
-      },
-    },
-  })
-  await db.projects.upsert({
-    where: { name: "WizeRunner" },
-    update: {},
-    create: {
-      name: "WizeRunner",
-      owner: { connect: { id: b.id } },
-      description:
-        "With the current pandemic situation, people are advised not to gather in large numbers for any sporting events such as mass running tournaments. Moreover, many individuals do not necessarily have to compete with others since competing against themselves offers the greatest reward. From those reasons, we came up with the idea to build a system not only for Wizeliners but also for the community so that everyone can stay motivated and boost their competitive spirit through running challenges and virtual races.",
-      valueStatement:
-        "Nowadays, there are a number of systems that allow users to participate in running challenges or participate in virtual races with some limitations. The first drawback is that most of these systems are not free. Users have to pay to be able to participate in virtual races or to be able to join public or private running challenges individually or with a team. The second limitation is that some of the paid systems, such as the ChallengeRunner, have a rather monotonous and boring design and reporting. That may reduce the users' motivation when using the app. Finally, most systems only support one of two functions, either participating in running challenges or participating in virtual races. In other words, to use both of those functions, users have to use and switch between two separate systems which is complex and costly. That motivates us to build a centralized system for Wizeliners to improve physical and mental health through virtual running functions.",
-      target: "All wizeliners",
-      projectStatus: { connect: { name: "Idea Submitted" } },
-      searchSkills: "Java, PostgreSQL, React",
-      skills: {
-        connect: [
-          { id: "f88e6170-9382-4a89-87a4-b820e4b0ac33" },
-          { id: "d7e95b76-6fc0-4e11-9674-61233cbe60ca" },
-          { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
-        ],
-      },
-      labels: { connect: [{ name: "Innovation Camp 2021" }] },
-      projectMembers: {
-        create: [
-          {
-            profile: { connect: { id: b.id } },
-            hoursPerWeek: 3,
-            role: "Tech Lead",
-          },
-        ],
-      },
-      stages: {
-        create: projectStages,
-      },
-    },
-  })
-  await db.projects.upsert({
-    where: { name: "Sorting Cat / Communities Platform" },
-    update: {},
-    create: {
-      name: "Sorting Cat / Communities Platform",
-      owner: { connect: { id: enoc.id } },
-      description:
-        "We want to create a new platform where wizeliners can see all the existing communities and send a request to form part of the one they like the most. Community Champions can also manage their community information and see all the people within. ",
-      valueStatement: `Communities play an important role in creating a sense of belonging among engineers at Wizeline. With the new portfolio organization, the importance of the communities only grows, as they create an opportunity for wizeliners with similar interests to share experiences,  seek help, and learn new things.
+    })
+    await db.projects.upsert({
+      where: { name: "Sorting Cat / Communities Platform" },
+      update: {},
+      create: {
+        name: "Sorting Cat / Communities Platform",
+        owner: { connect: { id: enoc.id } },
+        description:
+          "We want to create a new platform where wizeliners can see all the existing communities and send a request to form part of the one they like the most. Community Champions can also manage their community information and see all the people within. ",
+        valueStatement: `Communities play an important role in creating a sense of belonging among engineers at Wizeline. With the new portfolio organization, the importance of the communities only grows, as they create an opportunity for wizeliners with similar interests to share experiences,  seek help, and learn new things.
 
       Before the portfolio re-organization, communities were grouped by discipline and office. As communities become a global entity within Wizeline, we realized there was no unique place for engineers to see the existing communities and have an automated process to get assigned to the community of their choice.
 
@@ -693,31 +697,34 @@ const seed = async () => {
       During the initial Innovation Camp sessions, where we pitched our idea in order to search for people to join the team, we realized there was a similar initiative for a community platform, which was developed by another team in Guadalajara. They had some work done but because of bandwidth, there was still a lot to get done in order to get the platform to be functional.
 
       We decided to join efforts and take the work that was already developed and finish the basic functionality so we can deliver this platform faster and gets ready to use in less time. `,
-      target: "All wizeliners",
-      projectStatus: { connect: { name: "Need SME Review" } },
-      searchSkills: "Go, MySQL, React",
-      skills: {
-        connect: [
-          { id: "8ea79390-240d-4b23-bd25-5eae45ac5132" },
-          { id: "f6d5b102-6cc4-41a8-8ca0-869aff695485" },
-          { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
-        ],
+        target: "All wizeliners",
+        projectStatus: { connect: { name: "Need SME Review" } },
+        searchSkills: "Go, MySQL, React",
+        skills: {
+          connect: [
+            { id: "8ea79390-240d-4b23-bd25-5eae45ac5132" },
+            { id: "f6d5b102-6cc4-41a8-8ca0-869aff695485" },
+            { id: "b27f5e6c-4470-4f83-8fd6-dc097e127f44" },
+          ],
+        },
+        labels: { connect: [{ name: "Innovation Camp 2021" }] },
+        projectMembers: {
+          create: [
+            {
+              profile: { connect: { id: enoc.id } },
+              hoursPerWeek: 3,
+              role: "Tech Lead",
+            },
+          ],
+        },
+        stages: {
+          create: projectStages,
+        },
       },
-      labels: { connect: [{ name: "Innovation Camp 2021" }] },
-      projectMembers: {
-        create: [
-          {
-            profile: { connect: { id: enoc.id } },
-            hoursPerWeek: 3,
-            role: "Tech Lead",
-          },
-        ],
-      },
-      stages: {
-        create: projectStages,
-      },
-    },
-  })
+    })
+  } catch (e) {
+    console.log("Error:", e)
+  }
 }
 
 export default seed
