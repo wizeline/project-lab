c99d874cecb100ab7993198d504aef70419b093f
Martin Robledo <martin.robledo@wizeline.com>
2022-06-27 17:40:59 -0500

    showing modal when the user is one month away

--- app/membership/queries/checkMembership.ts
+++ app/membership/queries/checkMembership.ts
@@ -1,20 +1,28 @@
 import { paginate, resolver } from "blitz"
-import db, { Prisma } from "db"
+import db from "db"
+import { Prisma } from "@prisma/client"
+
+interface membershipOutput {
+  updatedAt: string
+}
 
 export default resolver.pipe(resolver.authorize(), async (userId: String) => {
-  let result = null
-  const date = new Intl.DateTimeFormat([], {
-    year: "numeric",
-    month: "long",
-    day: "2-digit",
-  }).format(new Date())
-  console.log("sadsad2324")
+  function dateDiffInDays(dateOne, dateTwo) {
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24
+    const utc1 = Date.UTC(dateOne.getFullYear(), dateOne.getMonth(), dateOne.getDate())
+    const utc2 = Date.UTC(dateTwo.getFullYear(), dateTwo.getMonth(), dateTwo.getDate())
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY)
+  }
+
+  let result
+  const today = new Date()
 
   if (userId) {
-    let lastContribution = new Date(
-      ` SELECT updateAt from ProjectMembers where projectId=${userId}`
-    )
-    console.log(date, lastContribution)
+    let query =
+      await db.$queryRaw<membershipOutput>`SELECT "updatedAt" from "ProjectMembers" where "profileId" = ${userId}`
+    let updatedAt = new Date(query[0].updatedAt)
+    result = dateDiffInDays(updatedAt, today)
   } else {
     return null
   }
